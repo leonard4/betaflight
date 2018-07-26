2f55e5fc465d77467af693f3c29416c0a693492f
Leonard Burns <leonard4@gmail.com>
2018-07-26 14:22:39 -0700

    Update target.mk
    Added HMC and QMC compass files

--- src/main/target/CLRACINGF4/target.mk
+++ src/main/target/CLRACINGF4/target.mk
@@ -5,4 +5,6 @@ TARGET_SRC = \
             drivers/accgyro/accgyro_mpu6500.c \
             drivers/accgyro/accgyro_spi_mpu6000.c \
             drivers/accgyro/accgyro_spi_mpu6500.c \
+            drivers/compass/compass_hmc5883l.c \
+            drivers/compass/compass_qmc5883l.c \
             drivers/max7456.c
